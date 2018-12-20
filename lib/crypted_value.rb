54565a1d4c8f50be273278c82747ac0783ef18f6
yalab <rudeboyjet@gmail.com>
2018-12-20 13:53:36 +0900

    initial implement

--- lib/crypted_value.rb
+++ lib/crypted_value.rb
@@ -1,5 +1,63 @@
 require "crypted_value/version"
+require 'bcrypt'
 
-module CryptedValue
-  # Your code goes here...
+class CryptedValue
+  DEFAULT_PEPPER = "default_pepper"
+  DEFAULT_STRETCHES = 11
+  class << self
+    def pepper=(s)
+      @pepper = s
+    end
+
+    def stretches=(n)
+      @stretches = n
+    end
+
+    def pepper
+      @pepper || DEFAULT_PEPPER
+    end
+
+    def stretches
+      @stretches || DEFAULT_STRETCHES
+    end
+  end
+
+  def initialize(value)
+    if value.length == 60 && value[0..3] == "$2a$"
+      @hashed_value = value
+    else
+      @orig_value = value
+      @hashed_value = to_hash(value)
+    end
+  end
+
+  def to_s
+    @hashed_value
+  end
+
+  def ==(value)
+    compare(value)
+  end
+
+  private
+  def to_hash(value)
+    ::BCrypt::Password.create("#{value}#{self.class.pepper}", cost: self.class.stretches).to_s
+  end
+
+  def compare(value)
+    return false if @hashed_value.length < 1
+    bcrypt   = ::BCrypt::Password.new(@hashed_value)
+    value = "#{value}#{self.class.pepper}"
+    value = ::BCrypt::Engine.hash_secret(value, bcrypt.salt)
+    secure_compare(value, @hashed_value)
+  end
+
+  def secure_compare(a, b)
+    return false if a.length < 1 || b.length < 1 || a.bytesize != b.bytesize
+    l = a.unpack "C#{a.bytesize}"
+
+    res = 0
+    b.each_byte { |byte| res |= byte ^ l.shift }
+    res == 0
+  end
 end
--- test/crypted_value_test.rb
+++ test/crypted_value_test.rb
@@ -1,11 +1,24 @@
 require "test_helper"
 
 class CryptedValueTest < Minitest::Test
+  def setup
+    CryptedValue.pepper = "pepper"
+    CryptedValue.stretches = 11
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::CryptedValue::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_compare
+    crypted_value = CryptedValue.new("ATSUSHI YOSHIDA")
+    assert_match /\$2a\$11\$.+/, crypted_value.to_s
+    assert crypted_value == "ATSUSHI YOSHIDA"
+    assert crypted_value != "yalab"
+  end
+
+  def test_compare_from_crypted
+    crypted_value = CryptedValue.new("$2a$11$QsqgvMOGbH0CcUHVzSifKevmAlycAsN5zIdaQQBIg7kkmZNOrc27y")
+    assert crypted_value, "ATSUSHI YOSHIDA"
   end
 end
